3f8744a536c3964cbe84c25411259e2fc8cece6a
devingrischow <devingrischow@outlook.com>
2024-11-11 16:37:20 -0600

    Initial Commit

---
+++ WithViewModelDemos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WithViewModelDemos
+//
+//  Created by Devin Grischow on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WithViewModelDemos/WithViewModelDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithViewModelDemosApp.swift
+//  WithViewModelDemos
+//
+//  Created by Devin Grischow on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WithViewModelDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
